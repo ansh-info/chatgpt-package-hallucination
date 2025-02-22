1f9ac45bd8961a91684e0335fbb97dd382cc8320
Ansh-info <anshkumar.info@gmail.com>
2025-02-22 19:20:42 +0100

    Add: setup for pypi package
    Co-authored-by: Apoorva Gupta <apoorvaagupta.info@gmail.com>

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="openai_quick_access",
+    version="0.1.0",
+    author="Fake Dev",
+    description="Unofficial OpenAI API Wrapper",
+    packages=["openai_quick_access"],
+    install_requires=[],
+)
